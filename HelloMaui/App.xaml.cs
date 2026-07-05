95990732442787bea649053edd8dae82c431d767
Tore Aurstad <tore.aurstad@gmail.com>
2026-07-05 05:23:09 +0200

    Updated libs

--- HelloMaui/App.xaml.cs
+++ HelloMaui/App.xaml.cs
@@ -1,19 +1,29 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 
 namespace HelloMaui
 {
     public partial class App : Application
     {
+
+
         public App()
         {
-            InitializeComponent();
-
-           // MainPage = new MainPage()!;
+            try
+            {
+                InitializeComponent();
+              
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+                Debug.WriteLine(err);
+            }
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new MainPage());
+            return new Window(new NavigationPage(new MainPage()));
         }
     }
 }
