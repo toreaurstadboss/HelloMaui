8e1d888139558ff03dd3b4d350bb4192e0fa62f4
Tore Aurstad <tore.aurstad@gmail.com>
2026-07-07 06:19:12 +0200

    Added some page lifecycles

---
+++ HelloMaui/MainMenuPage.cs
@@ -0,0 +1,37 @@
+using CommunityToolkit.Maui.Markup;
+using HelloMaui.Demos.CollectionViews;
+
+namespace HelloMaui;
+
+public class MainMenuPage : ContentPage
+{
+    public MainMenuPage()
+    {
+        Title = "Main Menu";
+
+        var openDemoButton = new Button()
+            .Text("Open CollectionsViewDemo1");
+
+        openDemoButton.Clicked += async (_, _) =>
+        {
+            await Navigation.PushAsync(new CollectionsViewDemo1());
+        };
+
+        Content = new VerticalStackLayout
+        {
+            Spacing = 16,
+            Padding = new Thickness(24),
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Fill,
+            Children =
+            {
+                new Label()
+                    .Text("Hello Maui")
+                    .FontSize(28)
+                    .TextCenter(),
+
+                openDemoButton
+            }
+        };
+    }
+}
