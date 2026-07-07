0874a4d60769c1b03aa76d683ad26e8942febdfa
Tore Aurstad <tore.aurstad@gmail.com>
2026-07-07 05:17:16 +0200

    Added helper

---
+++ HelloMaui/Helpers/MauiStyleHelpers.cs
@@ -0,0 +1,27 @@
+﻿namespace HelloMaui.Helpers
+{
+
+    public class MauiStyleHelpers
+    {
+
+        public static T? GetApplicationStyle<T>(string resourceKey)
+        {
+            ArgumentNullException.ThrowIfNull(resourceKey);
+
+            if (Application.Current?.Resources?.TryGetValue(resourceKey, out object? style) == true)
+            {
+                if (style is T)
+                {
+                    return (T)style;
+                }
+
+                throw new InvalidCastException($"Resource with key '{resourceKey}' is not of type {typeof(T).FullName}");
+            }
+
+            return default;
+
+        }
+
+
+    }
+}
